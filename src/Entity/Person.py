b4d0575e88cc02389f4a98ae42155b8e3ffb02c0
Malam <malamdg@gmail.com>
2022-12-09 08:12:58 +0100

    finished person entity

--- src/Entity/Person.py
+++ src/Entity/Person.py
@@ -1,19 +1,7 @@
 from datetime import datetime
 
 
-def format_name(name: str):
-    new_name = name
-    spaces = name.__contains__(" ")
-    composed = name.__contains__("-")
-
-    if spaces:
-        new_name = " ".join(map(lambda el: el.strip().capitalize(), new_name.strip().split(" ")))
-
-    if composed:
-        new_name = "-".join(map(lambda el: el.strip().capitalize(), new_name.strip().split("-")))
-
-    return new_name
-
+# Person entity
 
 class Person:
     user_id: int
@@ -35,14 +23,56 @@ class Person:
     def get_first_name(self):
         return format_name(self.first_name)
 
+    def set_first_name(self, name: str):
+        self.first_name = name
+        return self
+
+    def get_last_name(self):
+        return self.last_name
+
+    def set_last_name(self, name: str):
+        self.last_name = name
+        return self
+
+    def get_birthdate(self):
+        return self.birthdate
+
+    def set_birthdate(self, date: datetime):
+        self.birthdate = date
+        return self
+
+    def get_age(self):
+        if self.age is None:
+            interval = datetime.now() - self.birthdate
+            self.set_age(interval.days // 365)
+        return self.age
+
+    def set_age(self, age: int):
+        self.age = age
+        return self
+
+    def get_create_time(self):
+        return self.create_time
+
     def get_full_name(self):
         name = format_name(self.first_name)
         surname = self.last_name.strip().upper()
         return name + " " + surname
 
-    def get_age(self):
-        interval = datetime.now() - self.birthdate
-        return interval.days // 365
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+# ||| Factory may be moved to a helper or smt like that later on  |||
+# |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
-    def get_birthdate(self):
-        return self.birthdate
+
+def format_name(name: str):
+    new_name = name
+    spaces = name.__contains__(" ")
+    composed = name.__contains__("-")
+
+    if spaces:
+        new_name = " ".join(map(lambda el: el.strip().capitalize(), new_name.strip().split(" ")))
+
+    if composed:
+        new_name = "-".join(map(lambda el: el.strip().capitalize(), new_name.strip().split("-")))
+
+    return new_name
